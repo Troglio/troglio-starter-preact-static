da520d89a156c3b5812e94d6b68f2c23cb011435
Jonarod <jonrodd@gmail.com>
2018-03-30 05:12:11 +0200

    update

--- src/components/all.js
+++ src/components/all.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { SiteData, RouteData, Link } from 'react-static'
-import Layout from "../../themes/blank/layouts/Default"
-import {Templates} from "../../themes/blank/index"
+import {Templates, Layouts} from "../../themes/troglio-app/index"
 
 
-export default (props) => {
- 
+export default (props) => { 
     if (props.preview){
         let Template = (props.routeData.template && Templates[props.routeData.template.toLowerCase()]) || Templates[props.routeData.kind]
+        let Layout = (props.routeData.layout && Layouts[props.routeData.layout.toLowerCase()]) || Layouts["default"]
+        
         return (
             <Layout site={props.siteData} >
                 <Template site={props.siteData} page={props.routeData}/>
             </Layout>
         )
     } else {
+
         return (
             <SiteData render={(siteData) => {
                 return (
-                    <RouteData render={(routeData) => {                
+                    <RouteData render={(routeData) => {
                         let Template = (routeData.page.template && Templates[routeData.page.template.toLowerCase()]) || Templates[routeData.page.kind]
+                        let Layout = (routeData.page.layout && Layouts[routeData.page.layout.toLowerCase()]) || Layouts["default"]
                         return (
                             <Layout site={siteData} >
                                 <Template site={siteData} page={routeData.page}/>
@@ -30,5 +32,3 @@ export default (props) => {
         )
     }
 }
-
-
